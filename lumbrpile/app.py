8491ccd36669cd437fa05c86c7756ec382d26c4c
mingkaic <mingkaichen2009@gmail.com>
2026-01-02 20:44:32 -0800

    add gunicorn to lumbrsink for productionization

--- lumbrpile/app.py
+++ lumbrpile/app.py
@@ -1,3 +1,6 @@
+import gevent.monkey
+gevent.monkey.patch_all()
+
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
@@ -10,8 +13,10 @@ template_dir = os.path.join(os.path.dirname(__file__), "templates")
 
 app = Flask(__name__, template_folder=template_dir)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logs.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins='*', async_mode='gevent')
 
 class LogEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,7 +35,7 @@ def receive_log():
 
     # Push to real-time dashboard
     socketio.emit('new_log', data)
-    return "OK", 200
+    return 'OK', 200
 
 @app.route('/')
 def dashboard():
@@ -38,22 +43,23 @@ def dashboard():
 
 @socketio.on('connect')
 def handle_connect():
-    print("Client connected, sending historical logs...")
+    print('Client connected, sending historical logs...')
     # Query all logs from the database
     history = LogEntry.query.order_by(LogEntry.id.desc()).all()
 
     # Convert DB objects to a list of dictionaries
     history_data = [{
-        "severity": log.severity,
-        "timestamp": log.timestamp,
-        "file": log.file,
-        "line": log.line,
-        "message": log.message
+        'severity': log.severity,
+        'timestamp': log.timestamp,
+        'file': log.file,
+        'line': log.line,
+        'message': log.message
     } for log in history]
 
     # Emit only to the sender (the newly connected browser)
     socketio.emit('log_history', history_data)
 
 if __name__ == '__main__':
-    with app.app_context(): db.create_all()
+    with app.app_context():
+        db.create_all()
     socketio.run(app, port=5000)
